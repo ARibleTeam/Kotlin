9ecbe06d30ac0ea67644a3b969c8f1cb2e13ec18
Рощупкин А. Ю. <alexseyroshupkin@yandex.ru>
2025-12-11 21:01:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/alexrosh/SettingsActivity.kt
@@ -0,0 +1,28 @@
+package com.example.alexrosh
+
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val shareBtn = findViewById<LinearLayout>(R.id.share_btn)
+        shareBtn.setOnClickListener {
+            Toast.makeText(this@SettingsActivity, "djkhsdfjkh", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
